b10b1f9d9bf606a7f37076d7d70606b4ea291502
Patrick-G-Oliver <patrickgriffitholiver@gmail.com>
2020-04-28 21:00:06 -0400

    data fetched and added to state in App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
+import ContentField from './components/ContentField';
 
 
 function App() {
 
-  const [nasaData, setNasaData] = useState([]);
+  const [nasaData, setNasaData] = useState([])
+
+  useEffect( () => {
+    axios.get("https://api.nasa.gov/planetary/apod?api_key=qyp5yZIi8Ij6pFkU5K84ADlXFdbZRJJd7HqzBaXS") 
+      .then(response => {
+        console.log('response', response.data)
+        setNasaData(response.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, []);
 
   return (
+    <div>
+      <ContentField
+        title = {nasaData.title}
+        img = {nasaData.hdurl}
+        description = {nasaData.explanation}
+      />
+    </div>
+    );
+  }
+  
+  export default App;
+
+    /*&
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
       </p>
     </div>
-  );
-}
-
-export default App;
+    */
+  
