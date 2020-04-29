66023e4ccfd0079e1bc597bdf30a119fac5a0116
Patrick-G-Oliver <patrickgriffitholiver@gmail.com>
2020-04-28 21:20:37 -0400

    first element rendered to browser

---
+++ src/components/ContentField.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ContentField = props => {
+    return (
+        <h1>{props.title}</h1>
+    );
+    return (
+        <p>{props.description}</p>
+    )
+};
+
+export default ContentField;
